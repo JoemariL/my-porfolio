ff2ac328989ef4d0a2f1dc525afa9535a65b7b91
Joemari Lopez <99390805+JoemariL@users.noreply.github.com>
2023-07-31 11:08:01 +0800

    Updated packages.json and added sources files
    - Modified page header
    - Added page footer
    - Added page title and icon

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -12,7 +12,7 @@ const socialLinks = [
 		url: 'https://github.com/JoemariL/',
 	},
 	{
-		id: 3,
+		id: 2,
 		icon: <FiLinkedin />,
 		url: 'https://www.linkedin.com/in/joemari-lopez-95409423a/',
 	},
@@ -24,7 +24,7 @@ const Footer = () => {
         <div className="pt-6 mt-20 border-t-2 border-slate-50">
             <div className="font-general-regular flex flex-col justify-center items-center mb-12 sm:mb-28">
                 <p className="text-xl text-primary-dark text-slate-50 mb-5">
-                    For more follow me on these links
+                    For more, follow me on these links
                 </p>
                 <ul className="flex gap-4 sm:gap-8">
 						{socialLinks.map((link) => (
@@ -32,7 +32,7 @@ const Footer = () => {
 								href={link.url}
 								target="__blank"
 								key={link.id}
-								className="text-gray-400 hover:text-indigo-500 dark:hover:text-indigo-400 cursor-pointer rounded-lg bg-gray-50 dark:bg-ternary-dark hover:bg-gray-100 shadow-sm p-4 duration-300"
+								className="text-gray-400 hover:text-indigo-500 cursor-pointer rounded-lg bg-gray-50 dark:bg-ternary-dark hover:bg-gray-100 shadow-sm p-4 duration-300"
 							>
 								<i className="text-xl sm:text-2xl md:text-3xl">
 									{link.icon}
--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -25,7 +25,7 @@ const Header = () => {
                         <div>
                             <Link to="/">
                                 {
-                                    <h1 className='text-slate-50 hover:text-yellow-100 font-general-medium text-4xl'>
+                                    <h1 className='text-slate-50 hover:text-indigo-500 font-general-medium text-4xl'>
                                         MyPortfolio
                                     </h1>
                                 }
@@ -88,21 +88,21 @@ const Header = () => {
             <div className="font-general-medium hidden m-0 sm:ml-4 mt-5 sm:mt-3 sm:flex p-5 sm:p-0 justify-center items-center shadow-lg sm:shadow-none">
                 <Link
                     to="/projects"
-                    className="block text-left text-lg text-slate-50 hover:text-yellow-100 sm:mx-4 mb-2 sm:py-2"
+                    className="block text-left text-lg text-slate-50 hover:text-indigo-500 sm:mx-4 mb-2 sm:py-2"
                     aria-label="Projects"
                 >
                     Projects
                 </Link>
                 <Link
                     to="/about"
-                    className="block text-left text-lg text-slate-50 hover:text-yellow-100 sm:mx-4 mb-2 sm:py-2"
+                    className="block text-left text-lg text-slate-50 hover:text-indigo-500 sm:mx-4 mb-2 sm:py-2"
                     aria-label="About Me"
                 >
                     About Me
                 </Link>
                 <Link
                     to="/contact"
-                    className="block text-left text-lg text-slate-50 hover:text-yellow-100 sm:mx-4 mb-2 sm:py-2"
+                    className="block text-left text-lg text-slate-50 hover:text-indigo-500 sm:mx-4 mb-2 sm:py-2"
                     aria-label="Contact"
                 >
                     Contact
